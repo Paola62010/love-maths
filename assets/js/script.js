16b65c6745d0bd3c419e423fb8b1215d668b39f2
Paola <paola62010@gmail.com>
2021-06-09 20:01:16 +0100

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,10 @@ document.addEventListener("DOMContentLoaded", function() {
             }
             else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             } 
         })
+        runGame("addition");
     }
 })
 
@@ -19,9 +20,17 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    }
+    else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknowngame type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -40,7 +49,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+"
 
 }
 
